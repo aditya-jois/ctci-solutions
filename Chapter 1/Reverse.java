bf1306ca8033e1ab9d4d3d13b69cceee45ab2e03
aditya-jois <aditya-jois@users.noreply.github.com>
2017-01-31 22:51:37 +0530

    Create Reverse.java

---
+++ Chapter 1/Reverse.java
@@ -0,0 +1,15 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+		String toReverse = "abcd\0";
+		char[] reversed = new char[toReverse.length()];
+		System.out.println(toReverse);
+		for (int i = toReverse.length() - 1, j = 0; i >= 0; i--,j++) {
+			reversed[i] = toReverse.charAt(j);
+		}
+		for (int i = 0; i < reversed.length; i++) {
+			System.out.print(reversed[i]);
+		}
+	}
+
+}
