81cb4838ec2e1e5298732318696b1cc29b062dc3
aditya-jois <aditya-jois@users.noreply.github.com>
2017-05-03 20:39:01 +0530

    Create Problem5.java

---
+++ Chapter 1/Problem5.java
@@ -0,0 +1,25 @@
+public class Problem5 {
+
+	public static void main(String[] args) {
+		String badString = "aabcccccaaa";
+		char compare = badString.charAt(0);
+		int count = 1;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i < badString.length(); i++) {
+			if (compare == badString.charAt(i)) {
+				count += 1;
+			} else {
+				sb.append(compare).append(count);
+				compare = badString.charAt(i);
+				count = 1;
+			}
+		}
+		sb.append(compare).append(count);
+		if (sb.length() > badString.length()) {
+			System.out.println(badString);
+		} else {
+			System.out.println(sb.toString());
+		}
+	}
+
+}
