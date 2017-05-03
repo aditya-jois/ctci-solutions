127cc4fce6b64c2a3a46703135cdd0fc7892645c
aditya-jois <aditya-jois@users.noreply.github.com>
2017-05-03 19:29:57 +0530

    Create Problem4.java

---
+++ Chapter 1/Problem4.java
@@ -0,0 +1,27 @@
+public class Problem4 {
+
+	public static void main(String[] args) {
+		String input = "Mr John Smith";
+		int extraSpaceCount = 0;
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == ' ') {
+				extraSpaceCount++;
+			}
+		}
+		int newLength = input.length() + extraSpaceCount * 2;
+		char[] inputArray = new char[newLength];
+		for (int i = input.length() - 1; i >= 0; i--) {
+			if (input.charAt(i) == ' ') {
+				inputArray[newLength - 1] = '0';
+				inputArray[newLength - 2] = '2';
+				inputArray[newLength - 3] = '%';
+				newLength -= 3;
+			} else {
+				inputArray[newLength - 1] = input.charAt(i);
+				newLength -= 1;
+			}
+		}
+		System.out.println(inputArray);
+	}
+
+}
